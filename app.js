952389b833caece1fc22cbacf559b88c14f1eb6e
Melrizzuto <melrizzuto4@gmail.com>
2024-12-06 15:05:47 +0100

    created fn errorHandler and export in app.js - set up middleware error sistem js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const PORT = 3000;
 //importo i routers
 const postsRouter = require('./routers/postsRouter');
 const commentsRouter = require('./routers/commentsRouter');
+const errorsHandler = require('./middlewares/errorHandler');
 
+//middleware per body parsing
 app.use(express.json());
 
 // Configuro gli asset statici
@@ -16,16 +18,21 @@ app.get('/', (req, res) => {
     res.send('Server del mio blog');
 });
 
-// uso il router dei post
+//middleware per router
+
+//per posts
 app.use('/posts', postsRouter);
 
-// uso il router dei commenti
+//per commenti
 app.use('/comments', commentsRouter);
 
 // rotta fallback   
 app.all('*', (req, res) => {
     res.status(404).send(`<h1>Error 404. Page not found</h1>`)
 });
+
+//middleware per errore di sistema
+app.use(errorsHandler);
 
 // Avvio del server
 app.listen(PORT, () => {
